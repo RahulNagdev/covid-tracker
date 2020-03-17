e9492f8dd8dbd8191690583a8d99aab1ddecd4de
RahulNagdev <rahulnagdev980@gmail.com>
2020-03-18 02:43:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-czydsv-uwwk5r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-czydsv-uwwk5r)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NglModule } from 'ng-lightning';
+import { DemoPicklistBasic } from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule, NglModule ],
+  declarations: [ DemoPicklistBasic ],
+  bootstrap:    [ DemoPicklistBasic ],
+  providers   : [ ]
+})
+export class AppModule { }
